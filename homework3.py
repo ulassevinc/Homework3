46b55479670c8ef7b4a9f7ba9ce150dab5251567
ulassevinc <80682861+ulassevinc@users.noreply.github.com>
2021-05-04 19:05:35 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,22 @@
+txt = 'Ey Edip Adanada Pide Ye'
+
+x = txt.casefold()
+y = x.replace(" ", "")
+
+print(y[::-1])
+
+txt1 = 'When is it going to rain'
+
+a = txt1.casefold()
+b = a.replace(" ", "")
+
+print(b[::-1])
+
+
+
+
+
+
+
+
+
